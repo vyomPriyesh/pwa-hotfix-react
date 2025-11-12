986eeb71c9a2a0c63fb5d12e257de436cd8a4ae3
bhautik07449 <bhautikpokal7449@gmail.com>
2025-11-12 12:58:37 +0530

    user side changes

--- src/components/widgets/common_imgupload.jsx
+++ src/components/widgets/common_imgupload.jsx
@@ -14,7 +14,7 @@ const CommonImgupload = ({ value, onChange }) => {
         formData.append("images", file)
         const response = await AuthService.imageUpload(formData)
         if (response.data.success) {
-            onChange(IMG_URL+response.data.data[0])
+            onChange(IMG_URL + response.data.data[0])
         }
     };
     return (
@@ -24,7 +24,7 @@ const CommonImgupload = ({ value, onChange }) => {
                 className="h-16 w-16 lg:h-20 lg:w-20 xxl:h-24 xxl:w-24 overflow-hidden border border-border rounded-md cursor-pointer flex items-center justify-center"
             >
                 <img
-                    src={value}
+                    src={value || 'https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?semt=ais_hybrid&w=740&q=80'}
                     alt="Profile"
                     className="w-full h-full object-cover bg-center"
                 />
